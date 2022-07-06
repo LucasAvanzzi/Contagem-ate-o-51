cba06bfb693fa9b61115949ff5a4eae39b11757a
Avanci's <Avanci's@LAPTOP-HAJDSFCB>
2022-07-06 09:28:06 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicío 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicío_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 51; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
